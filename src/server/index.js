1f645b7167ed4171338b85f9be50832dd6e60078
vitkuz <vitkuz.info@gmail.com>
2017-08-31 01:00:42 +0300

    add server side rendering. fix index.html

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,15 @@
 var opn = require('opn');
 var path = require('path');
 var express = require('express');
+import fs from 'fs';
 
 import React from 'react';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { renderToString } from 'react-dom/server'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import {renderToString} from 'react-dom/server'
+import App from '../client/App'
+import netflixApp from '../client/reducers'
+
 
 var app = express();
 
@@ -14,45 +18,40 @@ const port = process.env.PORT || config.parsed.PORT;
 process.env.NODE_ENV = config.parsed.NODE_ENV;
 
 // This is fired every time the server side receives a request
-app.use(handleRender);
+
 
 function handleRender(req, res) {
     // Create a new Redux store instance
-    const store = createStore(counterApp)
-
-    // Render the component to a string
-    const html = renderToString(
-        <Provider store={store}>
-            <App />
-        </Provider>
-    )
-
+    const store = createStore(netflixApp)
     // Grab the initial state from our Redux store
-    const preloadedState = store.getState()
+
+
+    fs.readFile('./build/index.html', 'utf8', function (err, file) {
+        if (err) {
+            return console.log(err);
+        }
+
+        const preloadedState = store.getState()
+
+        // Render the component to a string
+        const html = renderToString(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )
+
+        const document = file.replace(/<div id="root"><\/div>/, `
+        <div id="root">${html}</div>
+        <script>       
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+        </script>`);
+        res.send(document);
+    });
 
     // Send the rendered page back to the client
-    res.send(renderFullPage(html, preloadedState))
+    // res.send(renderFullPage(html, preloadedState))
 }
 
-function renderFullPage(html, preloadedState) {
-    return `
-    <!doctype html>
-    <html>
-      <head>
-        <title>Redux Universal Example</title>
-      </head>
-      <body>
-        <div id="root">${html}</div>
-        <script>
-          // WARNING: See the following for security issues around embedding JSON in HTML:
-          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
-          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
-        </script>
-        <script src="/static/bundle.js"></script>
-      </body>
-    </html>
-    `
-}
 
 if (process.env.NODE_ENV !== 'production') {
     var webpackMiddleware = require("webpack-dev-middleware");
@@ -61,15 +60,16 @@ if (process.env.NODE_ENV !== 'production') {
     app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
     app.use(express.static('build'));
-    app.get('*', function (req, res) {
-        res.sendfile(path.join(__dirname, 'build/index.html'));
-    });
+
+    app.get('*', handleRender);
+
+    // app.get('*', function (req, res) {
+    //     res.sendfile(path.join(__dirname, 'build/index.html'));
+    // });
 }
 
 
-
 // app.set('view engine', 'pug');
-
 
 
 app.listen(port || 4000, function () {
