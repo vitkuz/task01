88623edac0562e7169043c04b25af58d89de6528
vitkuz <vitkuz.info@gmail.com>
2017-09-04 02:08:01 +0300

    implement redux promise

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
+import {fetchMovies} from "./actions/indeх"
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+
+
 
 class App extends Component {
+
+    componentDidMount() {
+        this.props.fetchMovies();
+    }
 
     render() {
         return (
             <div className="App">
-                App!!!!!!!!!
+                ReduxApp!!!!!!!!!
             </div>
         );
     }
 }
 
-export default App;
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchMovies},dispatch);
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(App)
