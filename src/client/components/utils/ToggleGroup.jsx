3fe50a5f5191cd3cf0dc3323f026ab0fe7852353
Vitali-Kuzmenka-EPAM <Vitali_Kuzmenka@epam.com>
2017-09-20 15:08:20 +0300

    final changes 2

--- src/client/components/utils/ToggleGroup.jsx
+++ src/client/components/utils/ToggleGroup.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Toggle from './ToggleButton';
 
 class ToggleGroup extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = { selected: props.selected };
         this.changeActive = this.changeActive.bind(this);
     }
-
     getToggleClassName(i) {
-        if ( this.state.selected === i) {
+        if (this.state.selected === i) {
             return 'active';
-        } else {
-            return '';
         }
+        return '';
     }
-
     changeActive(i) {
-        this.setState({selected:i});
+        this.setState({ selected: i });
     }
-
     renderButtons() {
         return this.props.buttons.map((button, index ) => {
-            if ((this.props.buttons.length-1) === index) {
-                return <Toggle updateSearchState={this.props.updateParent} key={index} index={index} text={button.title} addClass={this.getToggleClassName(index)} updateGroup={this.changeActive} />
-            } else {
-                return <Toggle updateSearchState={this.props.updateParent} key={index} index={index} text={button.title} addClass={this.getToggleClassName(index)+' mr1'} updateGroup={this.changeActive} />
-            }
+            <Toggle updateSearchState={this.props.updateParent}
+                    key={index}
+                    index={index}
+                    text={button.title}
+                    addClass={this.getToggleClassName(index)}
+                    updateGroup={this.changeActive} />
 
         });
     }
@@ -41,5 +39,9 @@ class ToggleGroup extends React.Component {
         );
     }
 }
+
+ToggleGroup.propTypes = {
+    selected: PropTypes.number
+};
 
 export default ToggleGroup;
