037e8e41afb9526ce745209fb136d75575a4679b
Vitali-Kuzmenka-EPAM <Vitali_Kuzmenka@epam.com>
2017-09-22 18:38:31 +0300

    more changes

--- src/client/pages/HomePage.jsx
+++ src/client/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import HeaderSearch from '../components/sections/HeaderSearch';
 import MovieGrid from '../components/results/MovieGrid';
 
-const HomePage = (props) => (
+const HomePage = props => (
     <div>
         <HeaderSearch updateSearchBy={props.updateSearchBy} searchByFlag={props.searchByFlag} handleSearch={props.handleSearch} />
         <MovieGrid database={props.database} updateSortBy={props.updateSortBy} sortByFlag={props.sortByFlag} />
