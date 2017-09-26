466112739b73d964f4088838929e8a83a122e9c1
Vitali-Kuzmenka-EPAM <Vitali_Kuzmenka@epam.com>
2017-09-26 12:25:10 +0300

    final changes router

--- src/client/components/results/Movie.jsx
+++ src/client/components/results/Movie.jsx
@@ -6,21 +6,23 @@ const Movie = (props) => {
     const url = `/movie/${props.movie.id}`;
 
     return (
-        <div className="col-md-3 mb1 movie">
-            <div className="movie-picture">
-                <Link to={url}>
-                    <img src={props.movie.img} alt="Movie Poster" />
-                </Link>
-            </div>
-            <div className="movie-description">
-                <div className="movie-description-year">
-                    Year: {props.movie.year}
+        <div className="col-md-3 mb1">
+            <div className="movie">
+                <div className="movie-picture">
+                    <Link to={url}>
+                        <img src={props.movie.img} alt="Movie Poster" />
+                    </Link>
                 </div>
-                <div className="movie-description-title mb1">
-                    <h3><Link to={url}>{props.movie.title}</Link></h3>
+                <div className="movie-description">
+                    <div className="movie-description-year">
+                        Year: {props.movie.year}
+                    </div>
+                    <div className="movie-description-title mb1">
+                        <h3><Link to={url}>{props.movie.title}</Link></h3>
+                    </div>
+                    <div className="movie-description-category">Cat: {props.movie.category}</div>
+                    <div className="movie-description-rating">Rating: {props.movie.rating}</div>
                 </div>
-                <div className="movie-description-category">Cat: {props.movie.category}</div>
-                <div className="movie-description-rating">Rating: {props.movie.rating}</div>
             </div>
         </div>
     );
--- src/client/components/results/MovieGrid.jsx
+++ src/client/components/results/MovieGrid.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Movie from './Movie';
 import Filters from '../filtering/Filters';
 
-
 function sortBy(key, reverse) {
     const moveSmaller = reverse ? 1 : -1;
     const moveLarger = reverse ? -1 : 1;
@@ -20,27 +19,52 @@ function sortBy(key, reverse) {
     };
 }
 
+function setMaxHeight() {
+    const elements = document.getElementsByClassName('movie');
+    let maxHeight = 0;
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.height = '';
+    }
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].offsetHeight > maxHeight) {
+            maxHeight = elements[i].offsetHeight;
+        }
+    }
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.height = `${maxHeight}px`;
+    }
+    return maxHeight;
+}
+
+
+let resizeTimeout;
+function resizeThrottler() {
+    if (!resizeTimeout) {
+        resizeTimeout = setTimeout(() => {
+            resizeTimeout = null;
+            setMaxHeight();
+        }, 66);
+    }
+}
+
 class MovieGrid extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             database: props.database,
             sortBy: props.sortByFlag,
+            maxHeight: '',
         };
     }
     componentDidMount() {
-        const elements = document.getElementsByClassName('movie');
-        const heights = [];
-        for (let i =0; i < heights.length; i++) {
-            elements[i].style.height;
-            console.log(elements[i].clientHeight, elements.length);
-        }
-        
-        heights.
-        // heights.each(item => {
-        //     console.log(item);
-        // });
+        console.log('Component did mount!');
+        window.onload = function () {
+            setMaxHeight();
+        };
+        window.addEventListener('resize', resizeThrottler, false);
+        setMaxHeight();
     }
+
     renderMovies() {
         const sorted = [...this.state.database];
 
@@ -56,7 +80,7 @@ class MovieGrid extends React.Component {
         }
 
         return sorted.map((movie) => {
-            return <Movie key={movie.id} movie={movie} height={450} />;
+            return <Movie key={movie.id} movie={movie} />;
         });
     }
 
