9d5abade8b4a436109e726733fb49ccc735568de
Vitali-Kuzmenka-EPAM <Vitali_Kuzmenka@epam.com>
2017-10-12 15:00:33 +0300

    fix error

--- src/client/reducers/reducer_movies.js
+++ src/client/reducers/reducer_movies.js
@@ -1,20 +1,25 @@
 import C from '../actions/constants';
 
+const initialState = {
+    byId: [],
+    allIds: [],
+};
+
 function findIndex(movie, state) {
     return state.findIndex((item) => {
         return movie.unit === item.unit;
     });
 }
 
-export default function (state = [], action) {
+export default function (state = initialState, action) {
     switch (action.type) {
         case C.POPULATE_MOVIES:
             if (Array.isArray(action.payload)) {
                 const filtered = action.payload.filter((item) => {
-                    console.log(findIndex(item, state) < 0);
-                    return findIndex(item, state) < 0;
+                    console.log(findIndex(item, state.byId) < 0);
+                    return findIndex(item, state.byId) < 0;
                 });
-                const merged = [...state, ...filtered];
+                const merged = [...state.byId, ...filtered];
                 const allIds = merged.map((item) => {
                     return item.unit;
                 });
