74aebb8cd8491791e63e38c7c7151b8e074bae39
Vitali-Kuzmenka-EPAM <Vitali_Kuzmenka@epam.com>
2017-11-23 12:48:13 +0300

    update reducer

--- __tests__/Reducers.test.js
+++ __tests__/Reducers.test.js
@@ -1,20 +1,25 @@
-import { normalize, schema } from 'normalizr';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import MoviesPreview from '../src/client/reducers/reducer_moviesPreview';
 import C from '../src/client/constants';
 
-const movie = new schema.Entity('movies');
-const movies = [movie];
-
 Enzyme.configure({ adapter: new Adapter() });
+
 
 describe('Test app', () => {
     it('test MoviesPreview reducer', () => {
-        const data = [{ id: 1, title: 'title 1' }, { id: 2, title: 'title 2' }, { id: 3, title: 'title 3' }];
-        const normalizedData = normalize(data, movies);
-        const action = { type: C.POPULATE_MOVIES, payload: normalizedData };
+        const moviesData = {
+            entities: {
+                movies: {
+                    1: { id: 1, title: 'title 1' },
+                    2: { id: 2, title: 'title 2' },
+                    3: { id: 3, title: 'title 3' },
+                },
+            },
+            result: [1, 2, 3],
+        };
+        const action = { type: C.POPULATE_MOVIES, payload: moviesData };
         expect(MoviesPreview(undefined, action)).toEqual({
             byId: {
                 1: { id: 1, title: 'title 1' },
